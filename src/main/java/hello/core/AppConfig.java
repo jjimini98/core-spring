9f023172e07438b2804a7e91f91fccfd54bc1871
jjimini98 <jjimini98@naver.com>
2022-04-08 22:39:32 +0900

    0408 appconfig 리팩토링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,22 +1,32 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
+// 역할과 구현이 잘 구분되어야함.
 public class AppConfig {
 
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository()); //memberservice는 memberserviceimpl을 사용할거야 ~~
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository(); //memberrepository는 memorymemberrepository를 쓸거야~
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
+    }
 
 }
