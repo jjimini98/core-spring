9f7735e60d671171a3877e270ff440b75e9cd273
jjimini98 <jjimini98@naver.com>
2022-04-12 21:15:37 +0900

    0412 appconfig 리팩토링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,31 +2,40 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // 역할과 구현이 잘 구분되어야함.
+@Configuration
 public class AppConfig {
 
-
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository()); //memberservice는 memberserviceimpl을 사용할거야 ~~
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository(); //memberrepository는 memorymemberrepository를 쓸거야~
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
+
 
 }
